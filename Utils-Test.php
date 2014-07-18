b551af2aad555c93fd608cb1b3737c8696bc4736
hana arib <biranah@gmail.com>
2014-07-18 14:15:37 +0000

    Fichier UtilsTest a été modifié

--- Utils-Test.php
+++ Utils-Test.php
@@ -9,14 +9,5 @@ class UtilsTest extends PHPUnit_Framework_TestCase{
 		$expected=array('chat','chien','souris');
 		$this->assertEquals($expected,$result);
 	}
-	public function testIsPremierWithPremier(){
-		$this->assertEquals(true,Utils::isPremier(3));
-		$this->assertEquals(true,Utils::isPremier(7));
-		$this->assertEquals(true,Utils::isPremier(13));
-	}
-		public function testIsNotPremierWithPremier(){
-		$this->assertEquals(false,Utils::isPremier(4));
-		$this->assertEquals(false,Utils::isPremier());
-		$this->assertEquals(false,Utils::isPremier(18));
-	}
+	
 }
